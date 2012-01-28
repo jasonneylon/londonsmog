e0635df626c792a4144ed22f70c7cb74cb0775c3
Jason Neylon <jason.neylon@uswitch.com>
2012-01-28 22:27:43 +0000

    Basic street view working

--- server.rb
+++ server.rb
@@ -7,12 +7,12 @@ require 'sinatra/contrib'
 require "haml"
 require "sass"
 require "sinatra/reloader" if development?
+require 'net/http'
+require 'rexml/document'
 
 require File.join(File.dirname(__FILE__), *%w[lib helpers])
 
 include Helpers
-
-
 
 configure do
   set :haml, { :attr_wrapper => '"', :format => :html5 }
@@ -20,6 +20,14 @@ end
 
 
 get "/" do
-  haml :index, :locals => { :title => "London's invisible smog - using Google streetview to see London pollution" }
+	url = "http://www.londonair.org.uk/london/rss/rssLaGroupXml.asp?lagroup=LAQNiPhone"
+
+	xml_data = Net::HTTP.get_response(URI.parse(url)).body
+
+	# extract event information
+	doc = REXML::Document.new(xml_data)
+	sitecode = "MY1"
+ 	site = REXML::XPath.first(doc, "//site[@sitecode='#{sitecode}']")
+	haml :index, :locals => { :title => "London's invisible smog - using Google streetview to see London pollution", :site => site }
 end
 
