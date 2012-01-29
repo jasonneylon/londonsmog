7ed3e72d934dffa810e1fb275784c99e1d7165f2
Jason Neylon <jason.neylon@uswitch.com>
2012-01-29 07:34:37 +0000

    Colors!

--- server.rb
+++ server.rb
@@ -20,11 +20,23 @@ configure do
 
  Sass::Plugin.compiler.add_template_location("./public/sass")
  use Sass::Plugin::Rack
- 
 end
 
 
 get "/" do
+	aqlevels = {
+	 "1" => "Low", 
+	 "2" => "Low", 
+	 "3" => "Low", 
+	 "4" => "Moderate", 
+	 "5" => "Moderate", 
+	 "6" => "Moderate", 
+	 "7" => "High", 
+	 "8" => "High", 
+	 "9" => "High", 
+	 "10" => "Very high", 
+	}
+
 	url = "http://www.londonair.org.uk/london/rss/rssLaGroupXml.asp?lagroup=LAQNiPhone"
 
 	xml_data = Net::HTTP.get_response(URI.parse(url)).body
@@ -33,6 +45,12 @@ get "/" do
 	doc = REXML::Document.new(xml_data)
 	sitecode = "MY1"
  	site = REXML::XPath.first(doc, "//site[@sitecode='#{sitecode}']")
-	haml :index, :locals => { :title => "London's invisible smog - using Google streetview to see London pollution", :site => site }
+ 	aqindex = site.elements.first.attributes["aqindex"] 
+ 	aqindex = params["aqindex"] if params["aqindex"]
+	haml :index, :locals => { 
+		:title => "London's invisible smog - using Google streetview to see London pollution", 
+		:site => site, 
+		:aqindex => aqindex,
+		:aqlevels => aqlevels }
 end
 
